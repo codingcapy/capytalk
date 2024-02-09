2ef3ccaa3d4a7c1e83204aa5d3d4823f5c38024e
Paul K <spkim0921@gmail.com>
2024-02-09 15:54:49 -0800

    20th commit
    height adjustment for outlet components

--- src/components/FriendProfile.jsx
+++ src/components/FriendProfile.jsx
@@ -13,7 +13,7 @@ export default function FriendProfile(props) {
     const [expanded, setExpanded] = useState(false);
 
     return (
-        <div className="px-5 border-2 border-slate-600 md:w-[900px] h-screen overflow-y-auto">
+        <div className="px-5 border-2 border-slate-600 md:w-[900px] h-[90vh] md:h-screen overflow-y-auto">
             <p className="py-5 text-xl">{props.friendName}</p>
             {!expanded && <button className="rounded-xl my-5 py-2 px-2 bg-slate-700 text-white" onClick={() => setExpanded(true)}>Start Chat</button>}
             {expanded && <form onSubmit={props.handleCreateChat} className="flex flex-col">
