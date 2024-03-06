7c4a0f25e5eabe0c94a27b2c836868769eb90755
Paul K <spkim0921@gmail.com>
2024-03-05 17:47:26 -0800

    26th commit
    fix long message overflow side scroll bug

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -34,21 +34,21 @@ export default function Messages(props) {
         <div className="px-5 border-2 border-slate-600 bg-slate-800 md:w-[900px] h-[90vh] md:h-screen overflow-y-auto">
             <div className="text-xl sticky top-0 bg-slate-800 py-5">{props.currentChat.title}</div>
             <div className="sticky top-16 bg-slate-800 py-5">+ Invite friend</div>
-            <div>
+            <div className="overflow-hidden">
                 {props.currentMessages.map((message) =>
                     <div key={message.messageId} className="py-2">
-                        <div className="flex"><div className="font-bold">{message.username}</div><div className="pl-2">on {message.date}</div></div><div>{message.content}</div>
+                        <div className="flex"><div className="font-bold">{message.username}</div><div className="pl-2">on {message.date}</div></div>
+                        <div className="overflow-wrap break-word">{message.content}</div>
                     </div>)}
                 <div ref={messagesEndRef} />
-                <div className={`py-10 bg-slate-800 sticky z-20 ${isMenuSticky ? "top-0" : "bottom-0"
-                    }`}>
-                    <form onSubmit={props.handleCreateMessage}>
-                        <div className="flex">
-                            <input type="text" id="content" name="content" placeholder="write a message" value={props.inputMessage} onChange={(e) => props.setInputMessage(e.target.value)} required className="py-2 px-2 my-2 rounded-xl md:min-w-96 text-black" />
-                            <button type="submit" className=" mx-2 px-5 rounded-xl bg-yellow-600 text-white">Send</button>
-                        </div>
-                    </form>
-                </div>
+            </div>
+            <div className={`py-10 bg-slate-800 sticky z-20 ${isMenuSticky ? "top-0" : "bottom-0"}`}>
+                <form onSubmit={props.handleCreateMessage}>
+                    <div className="flex">
+                        <input type="text" id="content" name="content" placeholder="write a message" value={props.inputMessage} onChange={(e) => props.setInputMessage(e.target.value)} required className="py-2 px-2 my-2 rounded-xl md:min-w-96 text-black" />
+                        <button type="submit" className="mx-2 px-5 rounded-xl bg-yellow-600 text-white">Send</button>
+                    </div>
+                </form>
             </div>
         </div>
     )
