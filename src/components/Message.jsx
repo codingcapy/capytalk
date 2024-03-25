173bbed7d2fc0a7e98aad0f12db60bdbf8415c6c
Paul <spkim0921@gmail.com>
2024-03-24 18:44:56 -0700

    62nd commit
    implementing profile pic

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -6,13 +6,13 @@ version: 1.0
 description: message component for CapyTalk client
  */
 
-
 import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import axios from "axios";
 import DOMAIN from "../services/endpoint";
 import { MdModeEditOutline } from "react-icons/md";
 import { FaTrashCan } from "react-icons/fa6";
+import profilePic from "/capypaul01.jpg";
 
 const socket = io("https://capytalk-server-production.up.railway.app");
 
@@ -60,24 +60,33 @@ export default function Message(props) {
 
     return (
         <div className="py-2 message-container group hover:bg-slate-600 transition-all ease duration-300">
-            {props.message.replyContent && <div className="text-gray-400 pb-1"><span className="font-bold">@{props.message.replyUsername}</span> {props.message.replyContent}</div>}
-            <div className="flex"><div className="font-bold px-1">{props.message.username}</div><div className="pl-2">on {props.message.date.slice(0, 10)} {props.message.date.slice(11, 19)}</div></div>
-            <div className="md:flex justify-between px-1">
-                {!editMode && <div>
-                    <div className="overflow-wrap break-word pb-1">{props.message.content}</div>
-                </div>}
-                {editMode && <form onSubmit={handleEditMessage}>
-                    <input type="text" name="content" id="content" value={messageContent} onChange={(e) => setMessageContent(e.target.value)} className="text-black" />
-                    <input name="messageid" id="messageid" defaultValue={`${props.message.messageId}`} className="hidden" />
-                    <button className="edit-btn cursor-pointer px-2 mr-1 bg-slate-800 rounded-xl">Edit</button>
-                    <button className="delete-btn cursor-pointer px-2 mx-1 bg-red-600 rounded-xl" onClick={() => setEditMode(false)}>Cancel</button>
-                </form>}
-                <div className=" edit-delete hidden group-hover:flex opacity-100 transition-opacity">
-                    {!editMode && <div onClick={() => setEditMode(true)} className="flex edit-btn cursor-pointer px-2 mr-1 bg-slate-800 rounded-xl hover:bg-slate-700 transition-all ease duration-300">Edit <MdModeEditOutline size={20} className="ml-2" /></div>}
-                    {!editMode && <form onSubmit={handleEditMessage}>
-                        <input name="content" id="content" defaultValue="[this message was deleted]" className="hidden" />
+            {props.message.replyContent && <div className="text-gray-400 pb-1">
+                <div className="flex"><img src={profilePic} className="w-[20px] h-[20px]  rounded-full mx-2" /><span className="font-bold pr-2">@{props.message.replyUsername}</span> {props.message.replyContent}</div>
+            </div>}
+            <div className="flex">
+                <img src={profilePic} className="w-[40px] h-[40px] rounded-full mr-2" />
+                <div>
+                    <div className="md:flex md:w-[790px] justify-between px-1">
+                        <div className="flex">
+                            <div className="font-bold px-1">{props.message.username}</div><div className="pl-2">on {props.message.date.slice(0, 10)} {props.message.date.slice(11, 19)}</div>
+                        </div>
+                        <div className=" edit-delete hidden group-hover:flex opacity-100 transition-opacity">
+                            {!editMode && <div onClick={() => setEditMode(true)} className="flex edit-btn cursor-pointer px-2 mr-1 bg-slate-800 rounded-xl hover:bg-slate-700 transition-all ease duration-300">Edit <MdModeEditOutline size={20} className="ml-2" /></div>}
+                            {!editMode && <form onSubmit={handleEditMessage}>
+                                <input name="content" id="content" defaultValue="[this message was deleted]" className="hidden" />
+                                <input name="messageid" id="messageid" defaultValue={`${props.message.messageId}`} className="hidden" />
+                                <button type="submit" className="flex delete-btn cursor-pointer px-2 mx-1 bg-red-800 rounded-xl hover:bg-red-600 transition-all ease duration-300">Delete <FaTrashCan size={20} className="ml-2 pt-1" /></button>
+                            </form>}
+                        </div>
+                    </div>
+                    {!editMode && <div>
+                        <div className="overflow-wrap break-word pb-1">{props.message.content}</div>
+                    </div>}
+                    {editMode && <form onSubmit={handleEditMessage}>
+                        <input type="text" name="content" id="content" value={messageContent} onChange={(e) => setMessageContent(e.target.value)} className="text-black" />
                         <input name="messageid" id="messageid" defaultValue={`${props.message.messageId}`} className="hidden" />
-                        <button type="submit" className="flex delete-btn cursor-pointer px-2 mx-1 bg-red-800 rounded-xl hover:bg-red-600 transition-all ease duration-300">Delete <FaTrashCan size={20} className="ml-2 pt-1" /></button>
+                        <button className="edit-btn cursor-pointer px-2 mr-1 bg-slate-800 rounded-xl">Edit</button>
+                        <button className="delete-btn cursor-pointer px-2 mx-1 bg-red-600 rounded-xl" onClick={() => setEditMode(false)}>Cancel</button>
                     </form>}
                 </div>
             </div>
